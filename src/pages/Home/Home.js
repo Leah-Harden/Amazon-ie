abd28095d8083d2b3edc2b1ea00516dd7cc01648
Leah <leahxharden@gmail.com>
2023-07-14 17:55:56 -0400

    making a card

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,12 +5,27 @@ import * as React from 'react';
 import Navindex from '../Nav/Navindex.js'
 import CarouselHome from './CarouselHome.js';
 
+
+import tea from './CardPhotos/tea.png';
+import tayors from './CardPhotos/tayors.png';
+
 function Home() {
 
     return (
         <>
             <Navindex />
-            <CarouselHome />
+            <div>
+                <CarouselHome />
+                <div>
+                    <h3>Pick up where you left off</h3>
+                    <div>
+                        <img src={tea} alt='' />
+                        <img src={tayors} alt='' />
+                        <img src={tea} alt='' />
+                        <img src={tayors} alt='' />
+                    </div>
+                </div>
+            </div>
         </>
 
     );

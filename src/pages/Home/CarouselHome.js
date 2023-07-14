c9bbb84e2c3648ebfa12485ce4a7f1ab6a52234d
Leah <leahxharden@gmail.com>
2023-07-14 14:11:57 -0400

    fixed Carousel

--- src/pages/Home/CarouselHome.js
+++ src/pages/Home/CarouselHome.js
@@ -1,45 +1,25 @@
 
-
 import Carousel from 'react-bootstrap/Carousel';
+
+import kids from './HomePhotos/kids.png';
+import house from './HomePhotos/house.png';
+import Alexa from './HomePhotos/alexa.png';
 
 function CarouselHome() {
     return (
         <>
             <Carousel>
                 <Carousel.Item interval={1000}>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=373940"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    <img className="d-block w-100" src={kids} alt="First slide" />
+
                 </Carousel.Item>
                 <Carousel.Item interval={500}>
                     <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                        className="d-block w-100" src={house} alt="Second slide" />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption>
+                        className="d-block w-100" src={Alexa} alt="Third slide" />
                 </Carousel.Item>
             </Carousel>
 

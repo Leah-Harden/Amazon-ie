2d50884861f01b975f323c565297bbc6927d391d
Leah <leahxharden@gmail.com>
2023-07-16 19:24:48 -0400

    changed card pick

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,9 @@ import * as React from 'react';
 import Navindex from '../Nav/Navindex.js'
 import CarouselHome from './CarouselHome.js';
 
+import cardPick from './Cards/cardPick.js';
+
+
 
 import tea from './CardPhotos/tea.png';
 import tayors from './CardPhotos/tayors.png';
@@ -18,37 +21,31 @@ function Home() {
                 <CarouselHome />
                 <div className='cardRow'>
 
-                    <div className='cardPick'>
-                        <h5 className='cardTitle'>Pick up where you left off</h5>
-                        <div>
-                            <img src={tea} alt='' />
-                            <img src={tayors} alt='' />
-                            <img src={tea} alt='' />
-                            <img src={tayors} alt='' />
-                        </div>
-                    </div>
+                    <cardPick />
+
+
                     <div className='cardPick'>
                         <h5 className='cardTitle'>Keep shopping for</h5>
                         <div className='cardGrid'>
                             <div className='cardView'>
-                                <img src={tea} alt='' />
-                                <h6>Titlehere</h6>
-                                <h6><span>2</span> viewed</h6>
+                                <img src={tea} alt='' className='photoPick' />
+                                <h6 className='titleHere'>Titlehere</h6>
+                                <h6 className='viewHere'><span>2</span> viewed</h6>
                             </div>
                             <div className='cardView'>
-                                <img src={tayors} alt='' />
-                                <h6>Titlehere</h6>
-                                <h6><span>2</span> viewed</h6>
+                                <img src={tayors} alt='' className='photoPick' />
+                                <h6 className='titleHere'>Titlehere</h6>
+                                <h6 className='viewHere'><span>2</span> viewed</h6>
                             </div>
                             <div className='cardView'>
-                                <img src={tea} alt='' />
-                                <h6>Titlehere</h6>
-                                <h6><span>2</span> viewed</h6>
+                                <img src={tea} alt='' className='photoPick' />
+                                <h6 className='titleHere'>Titlehere</h6>
+                                <h6 className='viewHere'><span>2</span> viewed</h6>
                             </div>
                             <div className='cardView'>
-                                <img src={tayors} alt='' />
-                                <h6>Titlehere</h6>
-                                <h6><span>2</span> viewed</h6>
+                                <img src={tayors} alt='' className='photoPick' />
+                                <h6 className='titleHere'>Titlehere</h6>
+                                <h6 className='viewHere'><span>2</span> viewed</h6>
                             </div>
                         </div>
                         <a href='#'>View your browsing history</a>
@@ -63,6 +60,8 @@ function Home() {
                             <img src={tea} alt='' />
                             <img src={tayors} alt='' />
                         </div>
+                        <p className='titleHere'>Shop your everyday essentials</p>
+                        <a href='#'>See more</a>
                     </div>
                 </div>
             </div>
@@ -72,3 +71,5 @@ function Home() {
 }
 
 export default Home;
+
+

93a7b58daa394d1731fe1df1616968818e457521
Leah <leahxharden@gmail.com>
2023-07-15 22:13:39 -0400

    updated cards

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,13 +16,53 @@ function Home() {
             <Navindex />
             <div className='homePageWidth'>
                 <CarouselHome />
-                <div className='cardPick'>
-                    <h5 className='cardTitle'>Pick up where you left off</h5>
-                    <div>
-                        <img src={tea} alt='' />
-                        <img src={tayors} alt='' />
-                        <img src={tea} alt='' />
-                        <img src={tayors} alt='' />
+                <div className='cardRow'>
+
+                    <div className='cardPick'>
+                        <h5 className='cardTitle'>Pick up where you left off</h5>
+                        <div>
+                            <img src={tea} alt='' />
+                            <img src={tayors} alt='' />
+                            <img src={tea} alt='' />
+                            <img src={tayors} alt='' />
+                        </div>
+                    </div>
+                    <div className='cardPick'>
+                        <h5 className='cardTitle'>Keep shopping for</h5>
+                        <div className='cardGrid'>
+                            <div className='cardView'>
+                                <img src={tea} alt='' />
+                                <h6>Titlehere</h6>
+                                <h6><span>2</span> viewed</h6>
+                            </div>
+                            <div className='cardView'>
+                                <img src={tayors} alt='' />
+                                <h6>Titlehere</h6>
+                                <h6><span>2</span> viewed</h6>
+                            </div>
+                            <div className='cardView'>
+                                <img src={tea} alt='' />
+                                <h6>Titlehere</h6>
+                                <h6><span>2</span> viewed</h6>
+                            </div>
+                            <div className='cardView'>
+                                <img src={tayors} alt='' />
+                                <h6>Titlehere</h6>
+                                <h6><span>2</span> viewed</h6>
+                            </div>
+                        </div>
+                        <a href='#'>View your browsing history</a>
+                    </div>
+
+
+                    <div className='cardPick'>
+                        <h5 className='cardTitle'>Buy Again</h5>
+                        <div>
+                            <img src={tea} alt='' />
+                            <img src={tayors} alt='' />
+                            <img src={tea} alt='' />
+                            <img src={tayors} alt='' />
+                        </div>
                     </div>
                 </div>
             </div>

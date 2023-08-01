a6ac20e52eb78ef01f8547e361ae3f42271008e4
Leah <leahxharden@gmail.com>
2023-08-01 18:13:20 -0400

    Create ProductIndex.js

---
+++ src/pages/Product/ProductIndex.js
@@ -0,0 +1,42 @@
+
+
+import Navindex from '../Nav/Navindex.js'
+
+
+function ProductIndex() {
+
+    return (
+        <>
+            <Navindex />
+            <div>
+                <a href='#'>Books</a>
+                <a href='#'>Literature & Fiction</a>
+                <a href='#'>Stuff</a>
+            </div>
+            <div>
+                <div>
+                    <img src={#} alt='' />
+                    <img src={#} alt='' />
+                    <img src={#} alt='' />
+                </div>
+                <img src={#} alt='' />
+                <div>
+                    <title>Lipton Black Tea Bag, 100 Count (Pack of 6)</title>
+                    <a href='#'>Visit the Lipton Store</a>
+                    <div>
+                        <p><span>4.8</span> </p>
+                        <a href='#'> <span>2,234</span>rating</a>
+                        <a href='#'><span>13</span>anwered questions</a>
+                    </div>
+                    <img src={#} alt='' />
+                    <hr></hr>
+                </div>
+
+            </div>
+
+
+        </>
+    )
+}
+
+export default ProductIndex;
